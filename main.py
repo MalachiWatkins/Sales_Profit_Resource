95b7fc3ccdcbbfe7d13eeb53bf905bc8636566a8
malachi.watkins3535@gmail.com <malachi.watkins3535@gmail.com>
2022-06-25 20:47:35 -0500

    WIP

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import time
 import pdb
 from statistics import mean
-#pdb.set_trace()
+# pdb.set_trace()
 
 ####################################################
 ############# TO-DO ############################
@@ -17,15 +17,16 @@ from statistics import mean
 ###################################################
 
 
-Bids = [] # Number of Bids
-Top_20 = [] # First 20 Results
-prices = [] # Price for all Listings
+Bids = []  # Number of Bids
+Top_20 = []  # First 20 Results
+prices = []  # Price for all Listings
 
-MinPrice = [] # Minimum Price for Listings
-AvgPrice = [] # Average Price for Listings
-HighestPrice = [] # Highest Price for Listings
-SellThrough = [] # Sell Through Percentage
+MinPrice = []  # Minimum Price for Listings
+AvgPrice = []  # Average Price for Listings
+HighestPrice = []  # Highest Price for Listings
+SellThrough = []  # Sell Through Percentage
 
+weight_cat_list = [MinPrice, AvgPrice, HighestPrice, SellThrough]
 ####################################################
 ############# Data Base ############################
 ####################################################
@@ -36,40 +37,46 @@ SellThrough = [] # Sell Through Percentage
 ############# Key word Flagging ####################
 ####################################################
 
-def keyword_flagging_Check(): # Checks if Current Keyword flagged
+
+def keyword_flagging_Check():  # Checks if Current Keyword flagged
 
     return
 
-def keyword_flagging_new(): # Create a new Kewword flag
+
+def keyword_flagging_new():  # Create a new Kewword flag
 
     return
 
 ####################################################
 ############# Grabs Key Data #######################
 ####################################################
+
+
 def get_key_data():
     ############# Global Vars for api payload ################
-    Key_word = "Test" # Keyword for Seacrch
-    pages_to_search = 5 # Number of Pages to Search
+    Key_word = "Test"  # Keyword for Seacrch
+    pages_to_search = 5  # Number of Pages to Search
     page_number = 1  # Starting Page Number
     ##########################################################
 
     ############ API Call ##############
-    while page_number < pages_to_search: # Search all pages up to pages_to_search
+    while page_number < pages_to_search:  # Search all pages up to pages_to_search
 
         #### Json Payload for searching API #####
-        Keyword_payload = {"isSize":False,"isWeddingCatagory":"False","isMultipleCategoryIds":False,"isFromHeaderMenuTab":False,"layout":"","searchText":Key_word,"selectedGroup":"","selectedCategoryIds":"","selectedSellerIds":"","lowPrice":"0","highPrice":"999999","searchBuyNowOnly":"","searchPickupOnly":"False","searchNoPickupOnly":"False","searchOneCentShippingOnly":"False","searchDescriptions":"False","searchClosedAuctions":"true","closedAuctionEndingDate":"6/16/2022","closedAuctionDaysBack":"150","searchCanadaShipping":"False","searchInternationalShippingOnly":"False","sortColumn":"1","page":page_number,"pageSize":"40","sortDescending":"true","savedSearchId":0,"useBuyerPrefs":"true","searchUSOnlyShipping":"true","categoryLevelNo":"1","categoryLevel":1,"categoryId":0,"partNumber":"","catIds":""}
+        Keyword_payload = {"isSize": False, "isWeddingCatagory": "False", "isMultipleCategoryIds": False, "isFromHeaderMenuTab": False, "layout": "", "searchText": Key_word, "selectedGroup": "", "selectedCategoryIds": "", "selectedSellerIds": "", "lowPrice": "0", "highPrice": "999999", "searchBuyNowOnly": "", "searchPickupOnly": "False", "searchNoPickupOnly": "False", "searchOneCentShippingOnly": "False", "searchDescriptions": "False",
+                           "searchClosedAuctions": "true", "closedAuctionEndingDate": "6/16/2022", "closedAuctionDaysBack": "150", "searchCanadaShipping": "False", "searchInternationalShippingOnly": "False", "sortColumn": "1", "page": page_number, "pageSize": "40", "sortDescending": "true", "savedSearchId": 0, "useBuyerPrefs": "true", "searchUSOnlyShipping": "true", "categoryLevelNo": "1", "categoryLevel": 1, "categoryId": 0, "partNumber": "", "catIds": ""}
 
-        response = requests.post('https://buyerapi.shopgoodwill.com/api/Search/ItemListing', json=Keyword_payload) # Json response from API
+        response = requests.post('https://buyerapi.shopgoodwill.com/api/Search/ItemListing',
+                                 json=Keyword_payload)  # Json response from API
         json_response = response.json()
 
         ######## Parse all listings and get the key data used for Prediction ########
         Results = json_response["searchResults"]
 
         for individual_listing in Results['items']:
-            Bids.append(individual_listing["numBids"]) # Gets Number of Bids
-            prices.append(individual_listing["currentPrice"]) # Get Prices
-            if len(Top_20) == 19: # Gets Top 20 Results for Dislpay
+            Bids.append(individual_listing["numBids"])  # Gets Number of Bids
+            prices.append(individual_listing["currentPrice"])  # Get Prices
+            if len(Top_20) == 19:  # Gets Top 20 Results for Dislpay
                 null = 'null'
             else:
                 Top_20.append(individual_listing)
@@ -79,7 +86,6 @@ def get_key_data():
 ####################################################
 ####### Parsing Data Needed for Weighting ##########
 ####################################################
-
 
 
 def weight():
@@ -109,36 +115,37 @@ def weight():
 ########## Return Prediction #######################
 ####################################################
 
+
 # THIS IS TEMP AND WILL BE A DJANGO REST API ONCE TESTING IS DONE
 Weight_Config = {
     'MinPrice': {
-    'Type': "USD",
-    'Total_Weight': 1.00,
-    'Zero': 5,
-    'Max': 15,
-    'Weight': {
-        'Value': 1,
-        'Added_Weight': 0.1,
+        'Type': "USD",
+        'Total_Weight': 1.00,
+        'Zero': 5,
+        'Max': 15,
+        'Weight': {
+            'Value': 1,
+            'Added_Weight': 0.1,
         }
     },
     'AvgPrice': {
-    'Type': "USD",
-    'Total_Weight': 5.00,
-    'Zero': 10 ,
-    'Max': 15 ,
-    'Weight': {
-        'Value': 1 ,
-        'Added_Weight': 1.00 ,
+        'Type': "USD",
+        'Total_Weight': 5.00,
+        'Zero': 10,
+        'Max': 15,
+        'Weight': {
+            'Value': 1,
+            'Added_Weight': 1.00,
         },
     },
     'HighestPrice': {
-    'Type': "USD",
-    'Total_Weight': 1.50,
-    'Zero': 10 ,
-    'Max': 15 ,
-    'Weight': {
-        'Value': 1 ,
-        'Added_Weight': 0.3 ,
+        'Type': "USD",
+        'Total_Weight': 1.50,
+        'Zero': 10,
+        'Max': 15,
+        'Weight': {
+            'Value': 1,
+            'Added_Weight': 0.3,
         }
     },
     'SellThrough': {
@@ -164,22 +171,25 @@ Weight_Config = {
 }
 
 Final_Weight = []
+
+
 def return_Prediction():
 
     ######## Min Weight ###############
+    for weight_cat in weight_cat_list:
+        print(weight_cat)
+        if weight_cat[0] == Weight_Config[weight_cat[0]]['Max']:  # Max Weight
+            Finish_Weight.append(Weight_Config[weight_cat]['Total_Weight'])
+        elif weight_cat[0] == Weight_Config[weight_cat]['Zero']:  # Min Weight
+            Final_Weight.append(0.00)
+        elif weight_cat[0] >= Weight_Config[weight_cat]['Zero']:  # Avg Weight
+            Weight_Parse = weight_cat[0] - Weight_Config[weight_cat]['Zero']
+            Weight_Parse_Final = Weight_Parse * \
+                Weight_Config[weight_cat]['Weight']['Added_Weight']
+            Final_Weight.append(Weight_Parse_Final)
 
-    if MinPrice[0] == Weight_Config['MinPrice']['Max']:
-        Final_Weight.append(Weight_Config['MinPrice']['Total_Weight'])
-    elif MinPrice[0] == Weight_Config['MinPrice']['Zero']:
-        Final_Weight.append(0.00)
-    elif MinPrice[0] >= Weight_Config['MinPrice']['Zero']:
-        Min_Weight_p_1 = MinPrice[0] - Weight_Config['MinPrice']['Zero']
-        Min_Weight_p_2 =  Min_Weight_p_1 * Weight_Config['MinPrice']['Weight']['Added_Weight']
-        Final_Weight.append(Min_Weight_p_2)
-    else:
-        Final_Weight.append(0.00)
-
-    ########### AVG Price ##############
+        else:
+            Final_Weight.append(0.00)
 
     # Sell Through percent is based on percentage not sold base weight accordingly
 
